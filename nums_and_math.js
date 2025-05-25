3e2394e2b5c3ed00f81bdb42cdb1640a740511f5
Vjain4323 <vjain4323@gmail.com>
2025-05-25 17:04:25 +0530

    add num and math

---
+++ nums_and_math.js
@@ -0,0 +1,37 @@
+const score = 100;
+const balance = new Number(1000.50);
+// console.log(balance);
+// console.log(balance.toString());
+// console.log(balance.toString().length);
+
+// console.log(balance.toFixed(2));
+
+const num1 = 100.448;
+
+// console.log(num1.toPrecision(5)); // "100"
+
+// const num2 = 100000;
+// console.log(num2.toLocaleString('en-US')); // "100,000"
+//console.log(num2.toLocaleString('en-IN')); // "1,00,000"
+
+
+// ====================================================================================
+
+
+// console.log(Math); 
+// console.log(Math.abs(-5)); // 5
+// console.log(Math.ceil(4.2)); // 5
+// console.log(Math.floor(4.8)); // 4
+// console.log(Math.round(4.5)); // 5
+// console.log(Math.max(1, 2, 3, 4, 5, 6)); // 6
+// console.log(Math.min(1, 2, 3, 4, 5, 6)); // 1
+// console.log(Math.random()); // Random number between 0 and 1
+// console.log(Math.sqrt(16)); // 4
+
+const randomNum = Math.floor(Math.random() * 100) + 1; // Random number between 1 and 100
+console.log(randomNum);
+min = 1;
+max = 6;
+console.log(Math.floor(Math.random() * (max - min + 1) + min)); // Random number between 1 and 6
+ 
+
